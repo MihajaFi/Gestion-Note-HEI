ea5774a59c94f45370e0661c239e670a0331010b
Fifaliana <hei.fifaliana@gmail.com>
2023-08-24 15:27:36 +0300

    feat : folder to test the API

---
+++ src/test/java/com/hei/noteheidemo/NoteHeiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hei.noteheidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteHeiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
