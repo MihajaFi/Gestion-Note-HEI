9d54f19a338b7f3c7cad8bb88e10b69272495245
Fifaliana <hei.fifaliana@gmail.com>
2023-08-24 22:08:01 +0300

    feat : add group controller for database operations related to groups

--- src/main/java/com/hei/noteheidemo/Controller/GroupController.java
+++ src/main/java/com/hei/noteheidemo/Controller/GroupController.java
@@ -1,2 +1,47 @@
-package com.hei.noteheidemo.Controller;public class GroupController {
+package com.hei.noteheidemo.Controller;
+
+import com.hei.noteheidemo.Entity.Group;
+import com.hei.noteheidemo.Service.GroupService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class GroupController {
+    private GroupService groupService ;
+    public GroupController(GroupService groupService) {
+        this.groupService = groupService;
+    }
+    @PostMapping("/group-create")
+    public ResponseEntity<String> createGroup(@RequestBody Group group) {
+            groupService.createGroup(group);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Group created successfully");
+
+    }
+
+    @GetMapping("/group")
+    public List<Group> getAllGroups() {
+        return groupService.getAllGroups() ;
+    }
+
+    @GetMapping("/group/{id}")
+    public Optional<Group> getGroupByID(@PathVariable int id){
+        return groupService.getGroupById(id) ;
+    }
+
+    @PutMapping("/group/{id}")
+    public Group updateGroup(@PathVariable int id, @RequestBody Group group) {
+        if (group.getId() != id){
+            throw new IllegalArgumentException("ID mismatch between URL and request body");
+        }
+        return groupService.updateGroup(group);
+    }
+
+    @DeleteMapping("/group/{id}")
+    public ResponseEntity<String> deleteStudent(@PathVariable int id) {
+        groupService.deleteGroup(id);
+        return ResponseEntity.ok("Student with ID " + id + " has been deleted.");
+    }
 }
