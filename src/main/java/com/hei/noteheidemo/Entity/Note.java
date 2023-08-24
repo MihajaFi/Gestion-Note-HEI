6c6b0e80bd521ed75ce77f761e00b0f3739e82b8
Fifaliana <hei.fifaliana@gmail.com>
2023-08-24 22:10:57 +0300

    feat : create note table and sequences

--- src/main/java/com/hei/noteheidemo/Entity/Note.java
+++ src/main/java/com/hei/noteheidemo/Entity/Note.java
@@ -1,2 +1,13 @@
-package com.hei.noteheidemo.Entity;public class Note {
+package com.hei.noteheidemo.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Note {
+    private int id ;
+    private int subject_id ;
+    private int student_id ;
+    private float note ;
 }
