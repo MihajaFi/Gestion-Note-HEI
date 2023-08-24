86a535f4cd260384cd6814f078f744a54a847535
Fifaliana <hei.fifaliana@gmail.com>
2023-08-24 22:11:25 +0300

    feat : create subject table and sequences

--- src/main/java/com/hei/noteheidemo/Entity/Subject.java
+++ src/main/java/com/hei/noteheidemo/Entity/Subject.java
@@ -1,2 +1,12 @@
-package com.hei.noteheidemo.Entity;public class Subject {
+package com.hei.noteheidemo.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Subject {
+    private int id  ;
+    private String subject_code ;
+    private int coef_subject ;
 }
