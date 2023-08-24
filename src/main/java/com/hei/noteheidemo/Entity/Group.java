cd71e1130c956c36eb0a9c90d011ad2d7e3ce356
Fifaliana <hei.fifaliana@gmail.com>
2023-08-24 22:10:45 +0300

    feat : create group table and sequences

--- src/main/java/com/hei/noteheidemo/Entity/Group.java
+++ src/main/java/com/hei/noteheidemo/Entity/Group.java
@@ -1,2 +1,15 @@
-package com.hei.noteheidemo.Entity;public class Group {
+package com.hei.noteheidemo.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Data
+@ToString
+@EqualsAndHashCode
+public class Group {
+    private int id  ;
+    private String name ;
 }
